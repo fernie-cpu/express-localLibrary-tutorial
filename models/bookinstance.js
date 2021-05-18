d64f120c6d8136c4645a6236e73d01547e27e116
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-18 18:46:03 -0300

    Add virtual property for date

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema({
@@ -14,8 +16,12 @@ var BookInstanceSchema = new Schema({
 });
 
 // Virtual for bookinstance's url
-BookInstanceSchema.virtual('url').get(() => {
-  `/catalog/bookinstance/${this._id}`;
+BookInstanceSchema.virtual('url').get(function () {
+  return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 // Exports model
