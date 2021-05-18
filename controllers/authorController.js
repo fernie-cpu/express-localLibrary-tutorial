33db2f99f486c28ce44ba217f6241688ac2493f4
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-18 19:15:34 -0300

    Author list controller function

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,8 +1,19 @@
 var Author = require('../models/author');
 
 // display list of all authors
-exports.author_list = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author list');
+exports.author_list = (req, res, next) => {
+  Author.find()
+    .sort([['family_name', 'ascending']])
+    .exec((err, list_authors) => {
+      if (err) {
+        return next(err);
+      }
+      // successful so render
+      res.render('author_list', {
+        title: 'Author List',
+        author_list: list_authors,
+      });
+    });
 };
 
 // display detail page for a specific
