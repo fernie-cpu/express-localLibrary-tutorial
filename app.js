353c0aea0679caadc00148df339d8d17f5907261
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-19 23:19:25 -0300

    database configuration

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express = require('express');
 
 // set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB =
+var dev_db_url =
   'mongodb+srv://ferniedb:ferniedatabase@cluster0.awbdf.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
