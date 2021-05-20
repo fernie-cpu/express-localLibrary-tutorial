629b1b0468cd57e09975611fb773e6d4e404a8d1
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-19 23:02:16 -0300

    Install/Add compression package

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); // import router for 'catalog' area of site
+var compression = require('compression');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
